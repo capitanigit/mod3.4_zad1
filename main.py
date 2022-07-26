867be611126ff5db71a8a19b47fc4f1f4e39fdcd
Krzysiek Krasowski <Krzysiek.Krasowski@gmail.com>
2022-07-26 14:17:48 +0200

    Print sum of products

--- main.py
+++ main.py
@@ -10,3 +10,5 @@ for k in d:
     temp = [v.capitalize() for v in d[k]]
     print(f"Idę do {k.capitalize()} kupię tu naspępujące rzeczy {temp}")
     suma = suma + len(d[k])   
+print(f"W sumie kupuję {suma} produktów")
+print()
